1805ea15e01a9076031560954d4c481c4037d92b
Erick Pérez Castellanos <erick.red@gmail.com>
2013-01-15 21:16:30 -0500

    Rails new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alexandria::Application.config.secret_token = 'fd969a26e353de61ad99bf71a3ee3119022a6ab3d76d6da7418de4a773d22e370484bb90ca4c730a52c1fad9be472eea4a605426659f8b966cc716fd15a4ec7e'
