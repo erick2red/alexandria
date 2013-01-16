1e5e3912612be268ece02a6834bd2ea0e58ead39
Erick Pérez Castellanos <erick.red@gmail.com>
2013-01-15 21:36:18 -0500

    Added some validations to models.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ActiveRecord::Base
   attr_accessible :authors, :description, :editorial, :keywords, :title, :year
+
+  validates :authors, :description, :editorial, :keywords, :title, :year, :presence => true
+  validates :title, :uniqueness => true
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,4 +2,7 @@ class Loan < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
   attr_accessible :due, :last_renewal_on, :renewal_count
+
+  validates :book_id, :user_id, :due, :presence => true
+  validates :book_id, :uniqueness => { :scope => :user_id }
 end
